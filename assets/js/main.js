669c7d86299a0da6ec2d6568011a53937099582e
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-09-29 16:59:00 -0500

    [Add] - componentes de servicios

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 console.log('hola');
+
+/* Misma altura en título */
+let servicios = document.querySelectorAll('.flex-center');
+
+let alturas = [];
+servicios.forEach((servicio) => {
+	alturas.push(servicio.clientHeight);
+});
+
+let maxHeight = Math.max(...alturas);
+
+servicios.forEach((servicio) => {
+	servicio.style.minHeight = maxHeight + 'px';
+});
