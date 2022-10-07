97e1faf1fc0c092b359da5d8842beecebd2ef198
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-10-06 21:08:44 -0500

    [Add] - galería de imágenes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-console.log('hola');
-
-/* Misma altura en título */
 function sameHeight(selector) {
 	let elements = document.querySelectorAll(selector),
 		heights = [],
@@ -13,5 +10,56 @@ function sameHeight(selector) {
 	elements.forEach((element) => (element.style.minHeight = maxHeight + 'px'));
 }
 
-// sameHeight('[data-bakery="feature-title"]');
-sameHeight('.flex-centered');
+sameHeight('[data-bakery="feature-title"]');
+
+/* ::: SplideJS config ::: */
+new Splide('.splide', {
+	grid: {
+		dimensions: [
+			[1, 1],
+			[2, 2],
+			[1, 2],
+			[1, 1],
+		],
+		gap: {
+			row: '.5rem',
+			col: '.5rem',
+		},
+	},
+	role: 'group', // https://splidejs.com/guides/accessibility/
+	label: 'Galería con algunos de nuestros productos',
+	rewind: true,
+	live: true,
+	perPage: 2,
+	drag: 'free',
+	gap: '.5rem',
+	cover: true,
+	heightRatio: 0.5,
+	classes: {
+		pagination: 'splide__pagination custom-pagination',
+		page: 'splide__pagination__page custom-pagination-page',
+		arrow: 'splide__arrow custom-arrow',
+	},
+	i18n: {
+		prev: 'Diapositiva anterior',
+		next: 'Siguiente diapositiva',
+		first: 'Ir a la primera diapositiva',
+		last: 'Ir a la última diapositiva',
+		slideX: 'Ir a diapositiva %s',
+		pageX: 'Ir a la página %s',
+		play: 'Iniciar reproducción automática',
+		pause: 'Pausar reproducción automática',
+		select: 'Elige una diapositiva para mostrar',
+		slide: 'diapositiva',
+		slideLabel: '%s de %s',
+	},
+	mediaQuery: 'min',
+	arrows: false,
+	breakpoints: {
+		900: {
+			arrows: true,
+			perPage: 3,
+			heightRatio: 0.3,
+		},
+	},
+}).mount(window.splide.Extensions);
