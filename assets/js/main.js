1405dbc65af202960336c4eedf163f9c73bfe2b2
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-10-11 16:01:49 -0500

    [Add] - página de contacto

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,52 +52,72 @@ Splide.defaults = {
 	rewind: true,
 };
 
-new Splide('#splide-hero', {
-	type: 'loop',
-	fixedWidth: '100%',
-	fixedHeight: '46.875rem',
-	autoplay: true,
-	interval: 5000,
-	label: 'Galería banners acerca de nosotros',
-	breakpoints: {
-		900: {
-			fixedHeight: '',
-			arrows: true,
-			heightRatio: '0.5625',
-		},
-		1200: {
-			heightRatio: '0.4285',
-		},
-	},
-}).mount();
+// new Splide('#splide-hero', {
+// 	type: 'loop',
+// 	fixedWidth: '100%',
+// 	fixedHeight: '46.875rem',
+// 	autoplay: true,
+// 	interval: 5000,
+// 	label: 'Galería banners acerca de nosotros',
+// 	breakpoints: {
+// 		900: {
+// 			fixedHeight: '',
+// 			arrows: true,
+// 			heightRatio: '0.5625',
+// 		},
+// 		1200: {
+// 			heightRatio: '0.4285',
+// 		},
+// 	},
+// }).mount();
 
-new Splide('#splide-gallery', {
-	grid: {
-		dimensions: [
-			[1, 1],
-			[2, 2],
-			[1, 2],
-			[1, 1],
-		],
-		gap: {
-			row: '.5rem',
-			col: '.5rem',
-		},
-	},
-	label: 'Galería con algunos de nuestros productos',
-	perPage: 2,
-	drag: 'free',
-	gap: '.5rem',
-	heightRatio: 0.5,
-	classes: {
-		pagination: 'splide__pagination gallery-pagination',
-		page: 'splide__pagination__page gallery-pagination-page',
-	},
-	breakpoints: {
-		900: {
-			arrows: true,
-			perPage: 3,
-			heightRatio: 0.3,
-		},
-	},
-}).mount(window.splide.Extensions);
+// new Splide('#splide-gallery', {
+// 	grid: {
+// 		dimensions: [
+// 			[1, 1],
+// 			[2, 2],
+// 			[1, 2],
+// 			[1, 1],
+// 		],
+// 		gap: {
+// 			row: '.5rem',
+// 			col: '.5rem',
+// 		},
+// 	},
+// 	label: 'Galería con algunos de nuestros productos',
+// 	perPage: 2,
+// 	drag: 'free',
+// 	gap: '.5rem',
+// 	heightRatio: 0.5,
+// 	classes: {
+// 		pagination: 'splide__pagination gallery-pagination',
+// 		page: 'splide__pagination__page gallery-pagination-page',
+// 	},
+// 	breakpoints: {
+// 		900: {
+// 			arrows: true,
+// 			perPage: 3,
+// 			heightRatio: 0.3,
+// 		},
+// 	},
+// }).mount(window.splide.Extensions);
+
+/* ::: Banners ::: */
+class Banner extends HTMLElement {
+	constructor() {
+		super();
+	}
+
+	connectedCallback() {
+		let title = this.getAttribute('data-title'),
+			img = this.getAttribute('data-img');
+
+		this.innerHTML = `
+		<div class="banner" style="background-image: url('${img}')">
+			<h1 class="banner__title">${title}</h1>
+		</div>
+		`;
+	}
+}
+
+customElements.define('section-banner', Banner);
