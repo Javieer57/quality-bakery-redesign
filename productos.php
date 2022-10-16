835520ff6c8d983c13f09b57ba14c911c4c88322
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-10-16 16:06:51 -0500

    [Update] - estilos para cards de productos

--- productos.php
+++ productos.php
@@ -7,22 +7,24 @@
   <section-banner data-title="Pastelería Tradicional" data-img="./assets/img/hero/hero-cake.png"></section-banner>
 
   <!-- ::: Productos ::: -->
-  <div class="container grid grid-md-2 grid-lg-3" id="product-container">
+  <div class="container grid grid-md-2 grid-lg-3 justifyItems-center" id="product-container">
   </div>
 
 </main>
 
 <template id="template-product-card">
-  <div class="product-card justifySelf-center">
+  <div class="product-card">
     <div class="product-card__img">
       <img data-template="image" src=".." alt="..">
     </div>
+    <div class="product-card__bg">
+    </div>
     <div class="product-card__content">
       <h3 data-template="name" class="product-card__name">..</h3>
-      <p data-template="description">..</p>
-    </div>
-    <div class="product-card__price-tag">
-      <p><span class="hidden">Precio:</span> <span data-template="price">..</span></p>
+      <p data-template="description" class="product-card__desc">..</p>
+      <div class="product-card__price-tag">
+        <p><span class="hidden">Precio:</span> <span data-template="price">..</span></p>
+      </div>
     </div>
   </div>
 </template>
