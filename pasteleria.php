e899b164410d8c26d8792f287e9433817609536c
Javieer E <65838690+Javieer57@users.noreply.github.com>
2022-10-18 14:33:55 -0500

    [Update] - estilos en asymmtric-card

--- pasteleria.php
+++ pasteleria.php
@@ -9,70 +9,73 @@
   <div class="container">
     <div class="asymmetric-card">
       <div class="asymmetric-card__thumb">
-        <img class="animate" src="./assets/img/pastel-tradicional.png" alt="" />
+        <img src="./assets/img/pastel-tradicional.png" alt="" />
       </div>
       <div class="asymmetric-card__content">
-        <h2 class="asymmetric-card__title animate"><a href="#">Pastelería tradicional</a></h2>
+        <h2 class="asymmetric-card__title "><a href="./productos.php">Pastelería tradicional</a></h2>
         <p class="asymmetric-card__text">
           Ven a deleitarte con la mayor variedad de postres artesanales y disfruta de las delicias que tenemos para ti.
           En nuestra pastelería encontrás los pasteles de siempre,
           elaborados con materia prima de la mejor calidad para garantizar el mejor sabor. ¡Tus pasteles y postres
           favoritos a unos pasos de tí!
         </p>
-        <button class="asymmetric-card__btn" aria-label="Ir a menú de pasteles tradicionales">
+        <a href="./productos.php" class="asymmetric-card__btn" aria-label="Ir a menú de pasteles tradicionales">
           <i class="fa-solid fa-arrow-right fa-fw"></i>
-        </button>
+        </a>
       </div>
     </div>
+
     <div class="asymmetric-card">
       <div class="asymmetric-card__thumb">
-        <img class="animate" src="./assets/img/pasteleria-fresas.png" alt="" />
+        <img src="./assets/img/pasteleria-fresas.png" alt="" />
       </div>
       <div class="asymmetric-card__content">
-        <h2 class="asymmetric-card__title animate"><a href="#">Pastelería gourmet</a></h2>
+        <h2 class="asymmetric-card__title "><a href="./productos.php">Pastelería gourmet</a></h2>
         <p class="asymmetric-card__text">
-          Sorprende a los tuyos con los mejores pasteles de la casa.  En Pastelería Quality Bakery  ponemos a tu
+          Sorprende a los tuyos con los mejores pasteles de la casa. En Pastelería Quality Bakery ponemos a tu
           disposición los pasteles más exquisitos, para los paladares más
-          exigentes. Ideales para compartir con amigos, o sorprender a un ser querido. Ven a disfrutar  de los pasteles
+          exigentes. Ideales para compartir con amigos, o sorprender a un ser querido. Ven a disfrutar de los pasteles
           Gourmet.
         </p>
-        <button class="asymmetric-card__btn" aria-label="Ir a menú de pasteles gourmet">
+        <a href="./productos.php" class="asymmetric-card__btn" aria-label="Ir a menú de pasteles gourmet">
           <i class="fa-solid fa-arrow-right fa-fw"></i>
-        </button>
+        </a>
       </div>
     </div>
+
     <div class="asymmetric-card">
       <div class="asymmetric-card__thumb">
-        <img class="animate" src="./assets/img/pastel-gourmet.png" alt="" />
+        <img src="./assets/img/pastel-gourmet.png" alt="" />
       </div>
       <div class="asymmetric-card__content">
-        <h2 class="asymmetric-card__title animate"><a href="#">Pasteles para eventos</a></h2>
+        <h2 class="asymmetric-card__title "><a href="./productos.php">Pasteles para eventos</a></h2>
         <p class="asymmetric-card__text">
-          Pastelería  Quality Bakery te ofrece la más rica solución a tus fiestas, tenemos los mejores pasteles para
+          Pastelería Quality Bakery te ofrece la más rica solución a tus fiestas, tenemos los mejores pasteles para
           cumpleaños, Bodas, Primera Comunión, Bautizos, Baby Shower
           y todo tipo de eventos. Deja tus fiestas en nuestras manos y no te arrepentirás.
         </p>
-        <button class="asymmetric-card__btn" aria-label="Ir a menú de pasteles para eventos">
+        <a href="./productos.php" class="asymmetric-card__btn" aria-label="Ir a menú de pasteles para eventos">
           <i class="fa-solid fa-arrow-right fa-fw"></i>
-        </button>
+        </a>
       </div>
     </div>
+
     <div class="asymmetric-card">
       <div class="asymmetric-card__thumb">
-        <img class="animate" src="./assets/img/pastel-navidad.png" alt="" />
+        <img src="./assets/img/pastel-navidad.png" alt="" />
       </div>
       <div class="asymmetric-card__content">
-        <h2 class="asymmetric-card__title animate"><a href="#">Fiesta decembrina</a></h2>
+        <h2 class="asymmetric-card__title "><a href="./productos.php">Fiesta decembrina</a></h2>
         <p class="asymmetric-card__text">
           Dale sabor a tus fiestas Decembrinas con los rico pasteles de Quality Bakery. Comparte con tu familia y amigos
-          los postres más tradicionales  que nos trae la época
+          los postres más tradicionales que nos trae la época
           más esperada del año y descubre las delicias que nuestras pastelerías tienen para ti. Ah! recuerda ….. ¡Si te
           tocó llevar el pastel nuestras pastelerías te harán
           quedar muy bien!
         </p>
-        <button class="asymmetric-card__btn" aria-label="Ir a menú de pasteles decembrinas">
+        <a href="./productos.php" class="asymmetric-card__btn" aria-label="Ir a menú de pasteles decembrinas">
           <i class="fa-solid fa-arrow-right fa-fw"></i>
-        </button>
+        </a>
       </div>
     </div>
   </div>
